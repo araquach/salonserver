c30c89987525f35f67ee7584ff022d684b2707ff
Adam Carter <adam@contra-design.com>
2022-05-20 18:15:22 +0100

    Fixed the ind reviews

--- handlers.go
+++ handlers.go
@@ -236,7 +236,7 @@ func apiReviews(w http.ResponseWriter, r *http.Request) {
 	if param == "All" {
 		DB.Where("salon = ?", salon).Limit(20).Find(&reviews)
 	} else {
-		DB.Where("salon = ?", salon).Where("stylist LIKE ?", "Staff: "+param+" %").Limit(20).Find(&reviews)
+		DB.Where("salon = ?", salon).Where("stylist LIKE ?", param+" %").Limit(20).Find(&reviews)
 	}
 
 	json, err := json.Marshal(reviews)
